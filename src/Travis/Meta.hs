0995cb84c78f9586c6cb6a1c5ed6bdb1954674b2
Oleg Grenrus <oleg.grenrus@iki.fi>
2015-08-13 16:51:39 +0300

    Use Data.Yaml.Pretty

--- src/Travis/Meta.hs
+++ src/Travis/Meta.hs
@@ -27,10 +27,13 @@ import Data.Char
 import Data.FileEmbed
 import Data.Maybe
 import Data.Monoid
+import Data.List as L (elemIndex)
+import Data.Function (on)
 import Data.Text as T
 import Data.Traversable
 import Data.Vector.Lens (vector)
 import Data.Yaml
+import Data.Yaml.Pretty
 import Prelude hiding (sequence)
 import Text.Regex.Applicative.Text
 
@@ -107,7 +110,7 @@ assertNoMatrixInclude v =
     Just v' -> Left $ "matrix.include specified: " ++ show v'
 
 preprocess :: ByteString -> Either String ByteString
-preprocess = fmap encode . preprocessYaml <=< decodeEither
+preprocess = fmap encode' . preprocessYaml <=< decodeEither
 
 preprocessIO :: FilePath -> FilePath -> IO ()
 preprocessIO source target = do
@@ -124,3 +127,18 @@ multiGhcTemplate = fromJust $ decode $(embedFile "data/language-haskell-multi-gh
 
 -- Right v <- decodeEither <$> BS.readFile ".travis.meta.yml"  :: IO (Either String Value)
 -- BS.putStr $ encode $ preprocessYaml v
+
+-- Nothing is smaller then Just
+-- We also swap params.
+elemIndexE :: Eq a => [a] -> a -> Either Int ()
+elemIndexE l e = maybe (Right ()) Left (L.elemIndex e l)
+
+listCompare :: Eq a => [a] -> a -> a -> Ordering
+listCompare l = compare `on` elemIndexE l
+
+preferredOrder :: [Text]
+preferredOrder = ["sudo", "language", "before_install", "install", "script", "matrix"]
+
+encode' :: ToJSON a => a -> ByteString
+encode' = encodePretty cfg
+  where cfg = setConfCompare (listCompare preferredOrder) defConfig
