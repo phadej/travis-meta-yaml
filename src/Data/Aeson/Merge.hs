3982463c55e935dc16167b5714466ad0324ff573
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-02-03 15:17:07 +0200

    Use aeson-extra-0.3.1.0

--- src/Data/Aeson/Merge.hs
+++ src/Data/Aeson/Merge.hs
@@ -6,10 +6,14 @@
 -- Maintainer  : Oleg Grenrus <oleg.grenrus@iki.fi>
 module Data.Aeson.Merge (merge) where
 
-import Data.Aeson.Types
+import Data.Aeson (Value)
+import qualified Data.Aeson.Extra.Merge as M
+import Data.Monoid ((<>))
 import Data.HashMap.Strict as HM
 
 -- | A bit like <http://underscorejs.org/#extend _.extend>, but pure, left-bias and recursive.
 merge :: Value -> Value -> Value
-merge (Object a) (Object b) = Object (HM.unionWith merge a b)
-merge a          _b         = a
+merge = M.merge f
+  where f r (M.ObjectF a) (M.ObjectF b) = M.ObjectF $ HM.unionWith r a b
+        f _ (M.ArrayF a)  (M.ArrayF b)  = M.ArrayF $ a <> b
+        f _  a            _             = a
